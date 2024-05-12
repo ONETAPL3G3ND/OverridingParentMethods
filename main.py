90333ac29ffc5bc532cfd0d9b59eb157fb273a15
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-05-12 14:23:43 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+class Test(dict):
+    def __init__(self, **kwargs):
+        print("innit")
+        super().__init__(kwargs)
+
+    def __setitem__(self, key, value):
+        print(f"setitem: {value}")
+        super().__setitem__(key, value * 3)
+
+if __name__ == '__main__':
+    t = Test(One=1, Two=2)
+    t["five"] = 5
+    print(t)
